4dd7cbac936361dbc28bc630169aeab5212e604e
Cristian Ninotti <cristianninotti03@gmail.com>
2024-06-25 20:54:43 -0300

    Push con fetch bien hecho

---
+++ js/common.js
@@ -0,0 +1,132 @@
+// const cotizaciones = [];
+
+// fetch("https://dolarapi.com/v1/dolares/oficial")
+//   .then(response => response.json())
+//   .then(dataDolarOficial => {
+//     cotizaciones.push(dataDolarOficial);
+//     return fetch("https://dolarapi.com/v1/dolares/blue");
+//   })
+//   .then(response => response.json())
+//   .then(dataDolarBlue => {
+//     cotizaciones.push(dataDolarBlue);
+//     return fetch("https://dolarapi.com/v1/dolares/bolsa");
+//   })
+//   .then(response => response.json())
+//   .then(dataDolarBolsa => {
+//     cotizaciones.push(dataDolarBolsa);
+//     return fetch("https://dolarapi.com/v1/dolares/contadoconliqui");
+//   })
+//   .then(response => response.json())
+//   .then(dataDolarCC => {
+//     cotizaciones.push(dataDolarCC);
+//     return fetch("https://dolarapi.com/v1/dolares/tarjeta");
+//   })
+//   .then(response => response.json())
+//   .then(dataDolarTarjeta => {
+//     cotizaciones.push(dataDolarTarjeta);
+//     return fetch("https://dolarapi.com/v1/dolares/mayorista");
+//   })
+//   .then(response => response.json())
+//   .then(dataDolarMayorista => {
+//     cotizaciones.push(dataDolarMayorista);
+//     return fetch("https://dolarapi.com/v1/dolares/cripto");
+//   })
+//   .then(response => response.json())
+//   .then(dataDolarCripto => {
+//     cotizaciones.push(dataDolarCripto);
+//     return fetch("https://dolarapi.com/v1/cotizaciones/eur");
+//   })
+//   .then(response => response.json())
+//   .then(dataEuro => {
+//     cotizaciones.push(dataEuro);
+//     return fetch("https://dolarapi.com/v1/cotizaciones/brl");
+//   })
+//   .then(response => response.json())
+//   .then(dataReal => {
+//     cotizaciones.push(dataReal);
+//     return fetch("https://dolarapi.com/v1/cotizaciones/clp");
+//   })
+//   .then(response => response.json())
+//   .then(dataChile => {
+//     cotizaciones.push(dataChile);
+//     return fetch("https://dolarapi.com/v1/cotizaciones/uyu");
+//   })
+//   .then(response => response.json())
+//   .then(dataUruguay => {
+//     cotizaciones.push(dataUruguay);
+
+//     // Una vez que todas las solicitudes han sido completadas, creas el objeto cotizacionesObjeto
+//     const cotizacionesObjeto = {
+//       dolarOficial: cotizaciones[0],
+//       dolarBlue: cotizaciones[1],
+//       dolarBolsa: cotizaciones[2],
+//       dolarCC: cotizaciones[3],
+//       dolarTarjeta: cotizaciones[4],
+//       dolarMayorista: cotizaciones[5],
+//       dolarCripto: cotizaciones[6],
+//       euro: cotizaciones[7],
+//       real: cotizaciones[8],
+//       chileno: cotizaciones[9],
+//       uruguayo: cotizaciones[10]
+//     };
+
+//     console.log(cotizacionesObjeto); // Aquí puedes hacer lo que quieras con los datos
+//     console.log(cotizaciones)
+//   })
+//   .catch(error => {
+//     console.error("Error al obtener datos de cotización:", error);
+//   });
+
+
+
+
+//   /*
+// var arrayCotizaciones = [];
+// var objetoCotizaciones = {};
+
+//   // Todos los fetchs por separado
+//   fetch 1 -- > URL1;
+// arrayCotizaciones.push(resultado);
+
+//   fetch 2 -- -> URL2;
+// arrayCotizaciones.push(resultado2);
+
+// */
+
+const arregloNuevo = []
+
+fetch("https://dolarapi.com/v1/dolares")
+  .then(response => response.json())
+  .then(dataDolar => {
+    console.log(dataDolar)  // Para ver el tipo de dato
+    arregloNuevo = dataDolar;  // Arreglo nuevo
+    return fetch("https://dolarapi.com/v1/cotizaciones/eur")
+  })
+  .then(response => response.json())
+  .then(dataEuro => {
+    arregloNuevo.push(dataEuro);
+    return fetch("https://dolarapi.com/v1/cotizaciones/brl");
+  })
+  .then(response => response.json())
+  .then(dataReal => {
+    arregloNuevo.push(dataReal);
+    return fetch("https://dolarapi.com/v1/cotizaciones/clp");
+  })
+  .then(response => response.json())
+  .then(dataChile => {
+    arregloNuevo.push(dataChile);
+    return fetch("https://dolarapi.com/v1/cotizaciones/uyu");
+  })
+  .then(response => response.json())
+  .then(dataUruguay => {
+    arregloNuevo.push(dataUruguay);
+  })  
+  .catch(error => {
+    console.error("Error al obtener datos de cotización:", error);
+  });
+
+  // en inicio se actualiza cada 5 minutos
+  // en mi archivo en local storage ( se guardan el historial de la cotizacion en favoritos en mi archivo del index a mi archivo)
+  // en mi informe se actualiza cada 5 minutos
+
+
